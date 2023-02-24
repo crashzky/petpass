52893554240518d89f6bbe2d6f69fd8ee56a0508
Кирилл Осинцев <osintsev.2000@inbox.ru>
2023-02-24 16:38:46 +0500

    fixed link on logo

--- layouts/MainLayout/index.tsx
+++ layouts/MainLayout/index.tsx
@@ -1,11 +1,14 @@
 import Props from './MainLayout.props';
 
 import MainLogo from '@assets/logos/mainLogo.svg';
+import Link from 'next/link';
 
 const MainLayout: React.FC<Props> = ({ className = '', contentClassName = '', children, ...props }) => {
 	return (
 		<div className={`${className} px-[100px] pt-14`}{...props}>
-			<MainLogo className='mb-11' />
+			<Link href='/'>
+				<MainLogo className='mb-11' />
+			</Link>
 			<div className={contentClassName}>
 				{children}
 			</div>
